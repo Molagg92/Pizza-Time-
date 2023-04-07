d66496e70f2c24ace3975e9580390a9d71b723eb
Erik Z <molagg92@gmail.com>
2023-04-07 15:10:52 -0700

    add code taht after much trial and error finally tracks the total costof a premade pizza correctly. I have not added a prototype method for this yet, but i have moved in the right direction

--- JS/scripts.js
+++ JS/scripts.js
@@ -19,26 +19,48 @@ const sizesArray = ["small", "medium", "large", "xl"]
 
 // //Pizza constructor logic------------------
 
-class  Pizza {
-  constructor ( size, crust, meat, topping ) {
+function  Pizza ( size, crust, meat, topping ) {
   this.size = size;
   this.crust = crust;
   this.meat = meat;
   this.topping = topping;  
-  return 'Order UP!' 
-  }
-};
+  return 'Order UP!' ;
+  };
+
+let classicAmerican = new Pizza ("medium", "golden", "pepperoni", "mushrooms");
+Pizza.prototype.chooseSize = function()
+ {
+console.log("Test")
+}
 
 //Price Tracker-------------------------------
 
 function priceTracker(order) {
-  const crustPrices ={
+  const sizePrices ={
   "small": 5.00,
   "medium": 10,
   "large": 13,
-  "xl": 15
+  "xl": 15,
   }
-  return crustPrices[order.size]
+  const crustPrices ={
+    "golden": 3,
+    "stuffed": 5,
+    "gluten-free": 7,
+    "thin": 3,
+  }
+  const meatPrices ={
+    "pepperoni": 2,
+    "sausage": 2,
+    "chicken": 2,
+    "anchovies": 2,
+  }
+  const toppingPrices = {
+    "mushrooms": 2,
+    "olives": 2,
+    "pineapple": 2,
+    "peppers": 2,
+  }
+  return sizePrices[order.size] + crustPrices[order.crust] + meatPrices[order.meat] + toppingPrices[order.topping]
 }
 
 
--- README.md
+++ README.md
@@ -13,3 +13,23 @@ Code:const myPizza = new Pizza([], "medium");
 Expected output: Pizza { toppings: [], size: "medium" }
 
 -----------------------
+
+
+Describe: priceTracker()
+Test:should keep accurate track of the cost of the size, crust, meats and toppings for the pizza being ordered
+Code:priceTracekr(order2)
+Expected output: $ 10
+
+-----------------------
+
+Test:should return the sum of size and crust
+Code: classicAmerican
+Expected output:$13
+
+-----------------------
+
+Test:should return the sum of all 4 perameters
+Code:
+Expected output:
+
+-----------------------
